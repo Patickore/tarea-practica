d64552de5911db693230e130d4658e7753cbd163
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-07-30 16:52:49 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tarea_practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int edad = 18;
+            string name = "Patrick";
+            Console.WriteLine("Su nombre es:" + name + "y su edad es:" + edad);
+        }
+    }
+}
